5caabfadbe5d5b5c9d357aee9d13743d1b4a2348
Imdadul Haque <tajbir2000@yahoo.com>
2020-10-30 08:45:41 +0600

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tajbir2000\MyFirstLaravelPackage\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Tajbir2000\MyFirstLaravelPackage\MyFirstLaravelPackageServiceProvider;
 
@@ -26,12 +27,13 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
+        Schema::dropAllTables();
+//        $app['config']->set('database.default', 'sqlite');
+//        $app['config']->set('database.connections.sqlite', [
+//            'driver' => 'sqlite',
+//            'database' => ':memory:',
+//            'prefix' => '',
+//        ]);
 
 
         include_once __DIR__.'/../database/migrations/create_my_first_laravel_package_table.php.stub';
